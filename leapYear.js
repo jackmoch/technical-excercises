7f6959ad3fa2ab78db87f4d41c6c463cd1724507
Jack Mocherman <mochermanjack@gmail.com>
2016-11-09 15:40:34 -0600

    imporved comment spacing

--- leapYear.js
+++ leapYear.js
@@ -10,7 +10,7 @@ const [ , , ...args] = process.argv;
 // or renders usage message if arguments passed incorrectly
 // Both returns increment year by 1 to avoid including current year in list
 
-let handleUserInput = (userInput) => {
+const handleUserInput = (userInput) => {
 	if (userInput.length === 0) {
 		return getCurrentYear() + 1 // Returns getCurrentYear function if no year is passed as an argument
 	} else if (userInput.length === 1) {
@@ -22,25 +22,29 @@ let handleUserInput = (userInput) => {
 }
 
 // A function that accepts a string as an argument and returns it as an integer
-let parseStringToNumber = (string) => {
+
+const parseStringToNumber = (string) => {
 	return parseInt(string, 10)
 }
 
 // A function that gets the current Date using the Javascript new Date() method
 // and returns the current year using the getFullYear method
-let getCurrentYear = () => {
+
+const getCurrentYear = () => {
 	const currentDate = new Date()
 	return currentDate.getFullYear()
 }
 
 // A function that accepts a year as an argument, returns true if it is a leap year
 // or returns false if it is not a leap year
-let isLeapYear = (year) => {
+
+const isLeapYear = (year) => {
 	return ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0))
 }
 
 // A function that accepts a year and returns the closest future leap year
-let determineClosestFutureLeapYear = (year) => {
+
+const determineClosestFutureLeapYear = (year) => {
 	if(!isLeapYear(year)) {
 		return determineClosestFutureLeapYear(year + 1) // If the year passed as an argument is not a leap year, increment and call recursively 
 	} else {
@@ -49,8 +53,9 @@ let determineClosestFutureLeapYear = (year) => {
 }
 
 // A function that accepts a leap year an argument and returns the upcoming leap year
-let determineNextLeapYearGivenCurrentLeapYear = (leapYear) => {
-	let potentialNextLeapYear = leapYear + 4 // Declares the potential next leap year by incrementing the passed year by four
+
+const determineNextLeapYearGivenCurrentLeapYear = (leapYear) => {
+	const potentialNextLeapYear = leapYear + 4 // Declares the potential next leap year by incrementing the passed year by four
 	if(!isLeapYear(potentialNextLeapYear)) {
 		return determineNextLeapYearGivenCurrentLeapYear(potentialNextLeapYear) // If the potentialLeapYear is not a leap year, the function is called again
 	} else {
@@ -60,13 +65,15 @@ let determineNextLeapYearGivenCurrentLeapYear = (leapYear) => {
 
 // A function that accepts a leap year and a leap year array, pushes the leap year into the leap year array,
 // and returns the leap year array
-let pushLeapYearToLeapYearArray = (leapYear, leapYearArray) => {
+
+const pushLeapYearToLeapYearArray = (leapYear, leapYearArray) => {
 	leapYearArray.push(leapYear)
 	return leapYearArray
 }
 
 // A function that accpets a leap year and a leap year array and builds a leap year array with a lenght of 20
-let buildLeapYearArray = (currentLeapYear, leapYearArray = []) => {
+
+const buildLeapYearArray = (currentLeapYear, leapYearArray = []) => {
 	if(pushLeapYearToLeapYearArray(currentLeapYear, leapYearArray).length !== 20) { // Checks if the array returned from calling the pushToLeapYearArray function has a length 																																										 of 20
 		const nextLeapYear = determineNextLeapYearGivenCurrentLeapYear(currentLeapYear) // Defines the next leap year using the determineNextLeapYearGivenCurrentLeapYear function
 		return buildLeapYearArray(nextLeapYear, leapYearArray) // Calls itself recursively until the leap year array has a length of 20
@@ -76,27 +83,31 @@ let buildLeapYearArray = (currentLeapYear, leapYearArray = []) => {
 }
 
 // A function that accepts an array of integers as an argument and returns an array of strings
-let convertArrayOfIntegersToArrayOfStrings = (arrayOfIntegers) => {
+
+const convertArrayOfIntegersToArrayOfStrings = (arrayOfIntegers) => {
 	return arrayOfIntegers.map((integer) => {
 		return integer.toString()
 	})
 }
 
 // A function that builds a single string from an array of strings
-let buildStringFromArrayOfStrings = (stringArray) => {
+
+const buildStringFromArrayOfStrings = (stringArray) => {
 	return stringArray.reduce((a, b) => {
 		return a + '\n' + b // Concatenates two strings, seperated by a new line character
 	})
 }
 
 // A function that prints a string with an ending new line character
-let printString = (string) => {
+
+const printString = (string) => {
 	process.stdout.write(string + '\n')
 }
 
 // A function accepts a user input and runs a program that prints out the next twenty leap years
 // based on the the user's input
-let runLeapYearProgram = (userInput) => {
+
+const runLeapYearProgram = (userInput) => {
 	const upcomingYear = handleUserInput(userInput) // Determines the upcoming year, based on the user's input
 	const firstLeapYear = determineClosestFutureLeapYear(upcomingYear) // Determines the first leap year in the leap year array based on the upcoming year
 	const leapYearIntegerArray = buildLeapYearArray(firstLeapYear) // Creates an array of leap years as integers using the buildLeapYearArray function with the first leap year 																																	of the array as its initial argument
@@ -106,4 +117,5 @@ let runLeapYearProgram = (userInput) => {
 }
 
 // Initiates the Leap Year Program
+
 runLeapYearProgram(args)
