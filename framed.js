084126679e4169b9f4a274c49e42f510ac28d019
Jack Mocherman <mochermanjack@gmail.com>
2016-11-10 11:33:46 -0600

    fixed up comments, added destructuring

--- framed.js
+++ framed.js
@@ -6,7 +6,7 @@
 
 const [, , ...args] = process.argv;
 
-// A function that accepts an array of words as an argument and returns an array with the lengths of those words
+// accepts an array of words as an argument and returns an array with the lengths of those words
 
 const buildArrayOfWordLengths = (wordArray) => {
 	return wordArray.map(word => {
@@ -14,30 +14,31 @@ const buildArrayOfWordLengths = (wordArray) => {
 	})
 }
 
-// A function that accepts an array of integers and returns the largest integer in the array
+// accepts an array of integers and returns the largest integer in the array
 
 const determineLargestNumberInArray = (wordLengthArray) => {
 	return Math.max(...wordLengthArray)
 }
 
-// A function that accepts an integer as it's argument and returns a string of asterisks whose length equals the integer + 4
+// accepts an integer as it's argument and returns a string of asterisks whose length equals the integer + 4
 
 const buildStringOfAsterisks = (longestWordLength) => {
 	return Array(longestWordLength + 5).join('*')
 }
 
-// A function that accepts a word array and the length of the longest word in the array as arguments and returns an array 
+// accepts a word array and the length of the longest word in the array as arguments and returns an array 
 // comprised of those words delimited by an asterisk on each side and seperated by a new line character
 // Each "word row" will be comprised of the same number of characters, with spaces accounting for differences in word length
+// destructures arguments from object passed in so they can be passed in any order
 
-const buildWordRowsArray = (wordArray, longestWordLength) => {
+const buildWordRowsArray = ({wordArray, longestWordLength}) => {
 	return wordArray.map(word => {
 		const spacerString = Array(longestWordLength - word.length + 1).join(' ')
 		return '* ' + word + spacerString + ' *' + '\n'
 	})
 }
 
-// A function that builds a single string from an array of strings
+// builds a single string from an array of strings
 
 const buildStringFromArrayOfStrings = (stringArray) => {
 	return stringArray.reduce((a, b) => {
@@ -45,9 +46,10 @@ const buildStringFromArrayOfStrings = (stringArray) => {
 	})
 }
 
-// A function that accepts a horizontal divider and a string of concatenated word rows and returns an array of those arguments
+// accepts a horizontal divider and a string of concatenated word rows and returns an array of those arguments
+// destructures arguments from object passed in so they can be passed in any order
 
-const buildFramedWordArray = (horiziontalDivider, wordRowsString) => {
+const buildFramedWordArray = ({horiziontalDivider, wordRowsString}) => {
 	const framedWordArray = []
 	framedWordArray.push(horiziontalDivider)
 	framedWordArray.push(wordRowsString)
@@ -55,7 +57,7 @@ const buildFramedWordArray = (horiziontalDivider, wordRowsString) => {
 	return framedWordArray
 }
 
-// A function that prints a string with an ending new line character
+// prints a string with an ending new line character
 
 const printString = (string) => {
 	process.stdout.write(string + '\n')
@@ -65,9 +67,9 @@ const runFramedProgram = (wordArray) => {
 	const wordLengthArray = buildArrayOfWordLengths(wordArray) // Builds an array used to determine the length of the longest word
 	const longestWordLength = determineLargestNumberInArray(wordLengthArray) // Determines the length of the longest word
 	const horiziontalDivider = buildStringOfAsterisks(longestWordLength) + '\n' // Builds a horizontal divider made of asterisks and adds a newline character to the end
-	const wordRowsArray = buildWordRowsArray(wordArray, longestWordLength) // Builds an array of the words input by the user, with each row in a frame
+	const wordRowsArray = buildWordRowsArray({wordArray, longestWordLength}) // Builds an array of the words input by the user, with each row in a frame
 	const wordRowsString = buildStringFromArrayOfStrings(wordRowsArray) // Creates a string of the framed word rows
-	const framedWordArray = buildFramedWordArray(horiziontalDivider, wordRowsString) // Builds an array of framed word rows w/ horizontal dividers in first and last positions
+	const framedWordArray = buildFramedWordArray({wordRowsString, horiziontalDivider}) // Builds an array of framed word rows w/ horizontal dividers in first and last positions
 	const framedWordString = buildStringFromArrayOfStrings(framedWordArray) // Converts the framed word array into a single string to be printed
 	printString(framedWordString) // Prints the finished framedWordString
 }
